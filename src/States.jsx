34343ed5ff732e954d3806c2e06ee31c9a21b028
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-05-26 19:42:44 +0530

    Added Graph

--- src/States.jsx
+++ src/States.jsx
@@ -1,12 +1,30 @@
 
 import React, { use } from "react";
 import { useParams } from "react-router-dom";
+import { BarChart } from '@mui/x-charts/BarChart';
 
 
 const States = () => {
     const id = useParams().id;
 
-    return <div>{id}</div>;
-};
+    return (
 
+        <div>
+            <BarChart
+                xAxis={[
+                    {
+                        id: 'barCategories',
+                        data: ['bar A', 'bar B', 'bar C'],
+                    },
+                ]}
+                series={[
+                    {
+                        data: [2, 5, 3],
+                    },
+                ]}
+                height={300}
+            />;
+        </div>
+    );
+}
 export default States;
